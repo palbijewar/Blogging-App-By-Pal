3706ae6269391f01ae62be77aba775342ee1faa3
Pal Bijewar <pal.bijewar@gmail.com>
2024-01-21 16:00:13 +0530

    pages and routes craeted

--- blogging-app/src/App.jsx
+++ blogging-app/src/App.jsx
@@ -1,9 +1,19 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Home,About,SignIn,SignUp,Dashboard,Projects} from "./pages";
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <>
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/about" element={<About/>} />
+    <Route path="/signin" element={<SignIn/>} />
+    <Route path="/signup" element={<SignUp/>} />
+    <Route path="/dashboard" element={<Dashboard/>} />
+    <Route path="/projects" element={<Projects/>} />
+   </Routes>
+   </BrowserRouter>
+   </>
   )
 }
