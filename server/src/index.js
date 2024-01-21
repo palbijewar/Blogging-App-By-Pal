b93d2271a52c5ffe49720e70fc794bcf98bb2e5a
Pal Bijewar <pal.bijewar@gmail.com>
2024-01-21 23:23:45 +0530

    server and database connected

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const app = express();
+
+mongoose.connect(`${process.env.MONGODB_URL}`).then(()=>console.log(`Database connected!`)).catch((error)=>console.log(error));
 
 app.listen(process.env.PORT || 3001, (req,res) => {
     console.log(`Server runing on port : ${process.env.PORT || 3001}`)
