d82d22e3f16e2b299855a8451b68699518d1b8ae
Pal Bijewar <pal.bijewar@gmail.com>
2024-01-21 23:29:10 +0530

    user schema created

---
+++ server/src/UserModel.js
@@ -0,0 +1,23 @@
+import mongoose , { Schema , model } from 'mongoose';
+
+const userSchema = new Schema({
+    username : {
+        type:String,
+        required:true,
+        unique:true
+    },
+    email : {
+        type:String,
+        required:true,
+        unique:true
+    },
+    password : {
+        type:String,
+        required:true,
+        unique:true
+    }
+}, {timestamps:true} )
+
+const users = model("Users", userSchema);
+
+export default users;
