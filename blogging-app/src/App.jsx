63cc77e8100a937182e5d5eafd7b279c0011e73b
Pal Bijewar <pal.bijewar@gmail.com>
2024-01-21 21:32:39 +0530

    header component created

--- blogging-app/src/App.jsx
+++ blogging-app/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {Home,About,SignIn,SignUp,Dashboard,Projects} from "./pages";
+import { Header } from "./components";
 
 export default function App() {
   return (
    <>
    <BrowserRouter>
+   <Header/>
    <Routes>
     <Route path="/" element={<Home/>} />
     <Route path="/about" element={<About/>} />
