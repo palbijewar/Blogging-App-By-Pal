993120e009dead28a34cf2f7760bd89d54e80f01
Pal Bijewar <pal.bijewar@gmail.com>
2024-01-27 15:32:52 +0530

    added functionality to to sign-up page

--- blogging-app/src/pages/SignUp.jsx
+++ blogging-app/src/pages/SignUp.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Label, TextInput, Button } from 'flowbite-react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Label, TextInput, Button, Alert, Spinner } from 'flowbite-react';
 'use client';
 
 export default function SignUp() {
+const [formData, setFormData] = useState({});
+const [errorMessage, setErrorMessage] = useState(null);
+const [loading, setLoading] = useState(false)
+const navigate = useNavigate();
+
+const handleChange = (e) => {
+ setFormData({...formData, [e.target.id]:e.target.value.trim()});
+};
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  if(!formData.username || !formData.password || !formData.email){
+return setErrorMessage('Please fill out all fields.')
+  }
+  try {
+    setLoading(true)
+    setErrorMessage(null)
+    const res = await fetch('http://localhost:3000/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify(formData),
+    });
+    
+    if (!res.ok) {
+      throw new Error(`Failed to create user: ${res.status} ${res.statusText}`);
+    }
+
+    const data = await res.json();
+    if(data.success === false){
+      return setErrorMessage(data.message);
+    }
+    setLoading(false);
+    if(res.ok){
+      navigate('/sign-in')
+    }
+  } catch (error) {
+    setErrorMessage(error.message);
+    setLoading(false);
+  }
+}
+
+
   return (
-    <div className='min-h-screen mt-20'>
+    <div className='min-h-screen mt-10'>
         <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'  >
       {/* left  */}
       <div className='flex-1' >
@@ -25,30 +70,48 @@ export default function SignUp() {
       <div className='flex-1'>
         {/* rigth */}
         <div className='flex flex-col gap-4' >
-        <form className='flex flex-col gap-4' >
+        <form className='flex flex-col gap-4' onSubmit={handleSubmit} >
   <div className="mb-4">
     <Label value="Your username"/>
-      <TextInput type="text" placeholder="Username" id="username1" />
+      <TextInput type="text" placeholder="Username" id="username" onChange={handleChange} />
   </div>
   <div className="mb-4">
     <Label value="Your email"/>
-      <TextInput type="text" placeholder="name@email.com" id="email" />
+      <TextInput type="email" placeholder="name@email.com" id="email" onChange={handleChange} />
   </div>
   <div className="mb-4">
     <Label value="Your password" />
-      <TextInput type="password" placeholder="Password" id="password" />
+      <TextInput type="password" placeholder="Password" id="password" onChange={handleChange} />
   </div>
   <Button
   gradientDuoTone='purpleToPink'
   type='submit'
+  disabled={loading}
   >
-   Sign Up
+{
+  loading ? (
+    <>
+    <Spinner size='sm'/>
+    <span className='pl-3' >Loading...</span>
+    </>
+  ) : 'Sign Up'
+}
   </Button>
   <div className='flex gap-2 text-sm mt-5' >
     <span> Have an account already ? </span>
     <Link to='/sign-in' className='text-blue-500' >
     Sign In
     </Link>
+    {
+      errorMessage && (
+        <Alert
+        className='mt-5' 
+        color='failure'
+        >
+         {errorMessage}
+        </Alert>
+      ) 
+    }
   </div>
 </form>
         </div>
--- server/src/index.js
+++ server/src/index.js
@@ -1,28 +1,35 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import userRoutes from './routes/users.routes.js'
+import userRoutes from './routes/users.routes.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-mongoose.connect(`${process.env.MONGODB_URL}`).then(()=>console.log(`Database connected!`)).catch((error)=>console.log(error));
+// Server-side CORS configuration
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
+
+mongoose.connect(`${process.env.MONGODB_URL}`).then(() => console.log(`Database connected!`)).catch((error) => console.log(error));
 
 app.use('/api/users', userRoutes);
 
 app.use((err, req, res, next) => {
- const statusCode = err.statusCode || 500;
- const message = err.message || "Internam server error";
- res.status(statusCode).json({
-    success:false,
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  res.status(statusCode).json({
+    success: false,
     statusCode,
-    message
- });
+    message,
+  });
 });
 
-app.listen(process.env.PORT || 3001, (req,res) => {
-    console.log(`Server runing on port : ${process.env.PORT || 3001}`)
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`Server running on port: ${process.env.PORT || 3001}`);
 });
