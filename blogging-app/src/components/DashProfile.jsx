845fefb2c0b8a001ec70d7f30b30c7d6e1522519
Pal Bijewar <pal.bijewar@gmail.com>
2024-02-19 10:53:43 +0530

    complete user image upload functionality

--- blogging-app/src/components/DashProfile.jsx
+++ blogging-app/src/components/DashProfile.jsx
@@ -1,16 +1,87 @@
-import { TextInput, Button } from 'flowbite-react'
-import React from 'react'
+import { TextInput, Button, Alert } from 'flowbite-react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase'
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function DashProfile() {
   const {currentUser} = useSelector(state => state.user)
+  const [imageFile, setImageFile] = useState(null)
+  const [imageFileUrl, setImageFileUrl] = useState(null)
+  const filePickerRef =  useRef()
+  const [imageFileUploadingProgress, setImageFileUploadingProgress] = useState(null)
+  const [imageFileUploadingError, setImageFileUploadingError] = useState(null)
+
+const handleImageChange = (e) => {
+  const file = e.target.files[0]
+  if(file){
+    setImageFile(file)
+    setImageFileUrl(URL.createObjectURL(file))
+  }
+};
+
+
+useEffect(() => {
+  if(imageFile){
+    uploadImage();
+  }
+}, [imageFile])
+
+const uploadImage = async () => {
+ const storage = getStorage(app);
+ const fileName = new Date().getTime() + imageFile.name
+ const storageRef = ref(storage, fileName)
+ const uploadTask = uploadBytesResumable(storageRef, imageFile)
+ uploadTask.on(
+  'state_changed',
+  (snapshot) => {
+    const progress = 
+    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+    setImageFileUploadingProgress(progress.toFixed(0))
+  },
+  (error) => {
+    setImageFileUploadingError('Could not upload image (file must be 2MB)!')
+    setImageFileUploadingProgress(null)
+    setImageFileUrl(null)
+  },
+  () => {
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+      setImageFileUrl(downloadURL)
+    })
+  },
+ )
+}
+
   return (
     <div className='max-w-lg mx-auto p-3 w-full' >
      <h1 className='my-7 text-center font-semibold text-3xl' >Profile</h1>
      <form className='flex flex-col gap-4 '>
-      <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full' >
-      <img src={currentUser.profile_picture} alt="user" className='rounded-full w-full h-full object-cover border-8 border-[lightgray]' />
-      </div>
+      <input type="file" accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden />
+      <div className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full' onClick={()=>filePickerRef.current.click()} >
+  <img src={imageFileUrl || currentUser.profile_picture} alt="user" className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${imageFileUploadingProgress && imageFileUploadingProgress<100 && 'opacity-60' } `} />
+  {imageFileUploadingProgress && (
+    <div className="absolute inset-0 flex items-center justify-center">
+      <CircularProgressbar
+        value={imageFileUploadingProgress || 0}
+        text={`${imageFileUploadingProgress}%`}
+        strokeWidth={5}
+        styles={{
+          root: { width: '100%', height: '100%' },
+          path: { stroke: `rgba(62, 152, 199, ${imageFileUploadingProgress / 100})` }
+        }}
+      />
+    </div>
+  )}
+</div>
+
+      {imageFileUploadingError &&  <Alert color='failure' >{}</Alert> }
       <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} />
       <TextInput type='text' id='email' placeholder='email' defaultValue={currentUser.email} />
       <TextInput type='text' id='password' placeholder='Password'  />
--- blogging-app/src/firebase.js
+++ blogging-app/src/firebase.js
@@ -1,20 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCy5MTfh-J87iRZG-0g4ClyKMGYzE8cY04",
-  authDomain: "blogging-app-4b94f.firebaseapp.com",
-  projectId: "blogging-app-4b94f",
-  storageBucket: "blogging-app-4b94f.appspot.com",
-  messagingSenderId: "362753028587",
-  appId: "1:362753028587:web:0aa2c6b5c209d6981229ae",
-  measurementId: "G-1XYRBBTV8S"
+  apiKey: "AIzaSyCmtqYMlerz0EU7d5fYPrIjE0qf_SJShg4",
+  authDomain: "blog-app-ffc50.firebaseapp.com",
+  projectId: "blog-app-ffc50",
+  storageBucket: "blog-app-ffc50.appspot.com",
+  messagingSenderId: "824069848339",
+  appId: "1:824069848339:web:6f800df9902c54481a6ee6",
+  measurementId: "G-K9KBD5MTB5"
 };
-  
+
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
