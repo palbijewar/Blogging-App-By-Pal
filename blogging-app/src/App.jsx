b224dd6d8a52ab062e5ff6c919b39ad262e84bd9
Pal Bijewar <pal.bijewar@gmail.com>
2024-02-23 13:26:22 +0530

    Complete post page functionality

--- blogging-app/src/App.jsx
+++ blogging-app/src/App.jsx
@@ -5,6 +5,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
 
 export default function App() {
   return (
@@ -24,6 +25,7 @@ export default function App() {
             <Route path="/dashboard" element={<Dashboard />} />
           </Route>
           <Route path="/projects" element={<Projects />} />
+          <Route path="/post/:postSlug" element={<PostPage />} />
         </Routes>
         <FooterComponent />
       </BrowserRouter>
