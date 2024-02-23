5fc4c727bf46a053cc4d97e324d6f309313ba661
Pal Bijewar <pal.bijewar@gmail.com>
2024-02-23 14:04:22 +0530

    Added callToAction component to the post page

--- blogging-app/src/pages/PostPage.jsx
+++ blogging-app/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from 'flowbite-react';
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import CallToAction from '../components/CallToAction';
 
 function PostPage() {
     const { postSlug } = useParams(); 
@@ -62,8 +63,11 @@ function PostPage() {
         className='p-3 max-w-2xl mx-auto w-full post-content'
         dangerouslySetInnerHTML={{__html: post && post.content}}
       >
-
+        </div>
+      <div className='max-w-4xl mx-auto w-full'>
+        <CallToAction />
       </div>
+  
     </main>
   )
 }
